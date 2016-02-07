ba6b4cb6ca6aced96a5f65917b02816a5ed686ab
Wolfie M <wwwolfie13@gmail.com>
2016-02-07 16:40:32 +0000

    Main Menu
    Thanks to @mrmarkwarner

---
+++ Assets/StartMenu.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class StartMenu : MonoBehaviour {
+
+	public Button playGame;
+	public Button exitGame;
+
+	public Camera menuCamera;
+	// Use this for initialization
+	void Start () {
+		playGame = playGame.GetComponent<Button> ();
+		exitGame = exitGame.GetComponent<Button> ();
+	}
+
+	public void StartLevel()
+	{
+		Application.LoadLevel ("AGDLevel");
+	}
+
+	public void ExitLevel()
+	{
+		Application.Quit ();
+	}
+}
