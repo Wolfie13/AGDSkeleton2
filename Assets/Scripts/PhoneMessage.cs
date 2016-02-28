a1822d027da1f44f185d81b6bf0794874d85e044
Wolfie13 <wwwolfie13@gmail.com>
2016-02-28 17:03:09 +0000

    Phone UI First Iteration
    Needs Integrating, Polishing, but all the API you could ever want should
    be there.

---
+++ Assets/Scripts/PhoneMessage.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PhoneMessage : MonoBehaviour {
+	public TextMesh messageText;
+	public GameObject background;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void destroy() {
+		Destroy (messageText);
+		Destroy (background);
+		Destroy (this);
+	}
+
+	public float getHeight() {
+		return background.renderer.bounds.extents.y * 2.0f;
+	}
+
+	public void setColor(Color c) {
+		Material mat = Instantiate (this.background.renderer.material) as Material;
+		mat.color = c;
+		this.background.renderer.material = mat;
+	}
+
+	public void setText(string s) {
+		messageText.text = s;
+		//Scale the text horizontally so it fits the screen.
+		{
+			float targetWidth = background.renderer.bounds.extents.x;
+			float currentWidth = messageText.renderer.bounds.extents.x;
+			float factor = messageText.transform.localScale.x * (targetWidth / currentWidth);
+			messageText.transform.localScale = new Vector3 (factor, factor, 1);
+		}
+
+		//Scale the message background vertically so it fits the text.
+		{
+			float targetHeight = messageText.renderer.bounds.extents.y;
+			float currentHeight = background.renderer.bounds.extents.y;
+			float factor = background.transform.localScale.y * (targetHeight / currentHeight);
+			background.transform.localScale = new Vector3 (background.transform.localScale.x, factor, 1);
+		}
+	}
+}
---
+++ Assets/Scripts/PhoneScreen.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class PhoneScreen : MonoBehaviour {
+	public PhoneMessage messagePrefab;
+
+	// Use this for initialization
+	void Start () {
+		displayMessage ("Hello, World", Color.blue);
+		displayMessage ("Goodbye, Cruel World", Color.red);
+		displayMessage ("AGD Spook Skeleton 2\nThe Spookening", Color.blue);
+		displayMessage ("#BlameHans\n#BlameWill", Color.red);
+		displayMessage ("I'm behind u and very...\nSpoookyyyyyy", Color.grey);
+		displayMessage ("MEMES", Color.black);
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	LinkedList<PhoneMessage> messages = new LinkedList<PhoneMessage>();
+
+	public void displayMessage(string msg, Color color) {
+		PhoneMessage newMessage = Instantiate (messagePrefab) as PhoneMessage;
+		newMessage.setText (msg);
+		newMessage.setColor (color);
+		messages.AddFirst (newMessage);
+		positionMessages ();
+	}
+
+	public void updateTime(string timeString) {
+		TextMesh time = GameObject.Find ("Time") as TextMesh;
+		if (time != null) {
+			time.text = timeString;
+		}
+	}
+
+	void positionMessages() {
+		float accumulatedHeight = -12.5f;
+		LinkedListNode<PhoneMessage> currentNode = messages.First;
+		while (currentNode != null) {
+			PhoneMessage currentMessage = currentNode.Value;
+			currentMessage.transform.parent = this.transform;
+			accumulatedHeight += (currentMessage.getHeight() / 2) + 0.1f;
+			currentMessage.transform.localPosition = new Vector3(0, accumulatedHeight, -2);
+			accumulatedHeight += (currentMessage.getHeight() / 2) + 0.1f;
+			Debug.Log(accumulatedHeight);
+			currentNode = currentNode.Next;
+			if (accumulatedHeight > 12) {
+				messages.Remove(currentMessage);
+				currentMessage.destroy();
+			}
+		}
+	}
+}
