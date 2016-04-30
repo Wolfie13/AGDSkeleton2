5e55d210278281ddba5e86d616bc9a1a6bfb3eb2
Will Marshall <wwwolfie13@gmail.com>
2016-04-30 17:33:52 +0100

    Player HP Check Fixed

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -12,7 +12,7 @@ public class Player : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (!debugMode) {
-			if (!debugMode) {
+			if (health <= 0) {
 				Application.LoadLevel ("DeathScreen");
 			}
 		}
