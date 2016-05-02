7a5d47aa3215453d0db2cb00fbbf39324b7d8dd4
Nathan Day <nathan@qawebhosting.com>
2016-05-02 20:37:37 +0100

    Stuff Wot Nathan Did

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -12,7 +12,7 @@ public class Player : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (!debugMode) {
-			if (!debugMode) {
+			if (!debugMode) { 
 				Application.LoadLevel ("DeathScreen");
 			}
 		}
